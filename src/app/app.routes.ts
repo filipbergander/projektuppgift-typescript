08bec2bc7c3b3bfefa8a4af59da552af52637d59
Filip Bergander <filip.bergander1996@gmail.com>
2026-05-06 11:07:51 +0200

    Favicon inom HTML. Lagt till routes för två komponenter, skapat 404-sida.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: Home},
+    {path: '', redirectTo: 'home', pathMatch:'full'},
+    {path: '404', component: NotFound},
+    {path: '**', redirectTo: '404', pathMatch:'full'}
+];
