14013843d9cc3ad7396445225e8d99ab89590435
Filip Bergander <filip.bergander1996@gmail.com>
2026-05-12 14:45:40 +0200

    Skapat länkar till undersidor under hero-bild.

--- src/app/partials/schema-links/schema-links.ts
+++ src/app/partials/schema-links/schema-links.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-schema-links',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIcon],
   templateUrl: './schema-links.html',
   styleUrl: './schema-links.scss',
 })
