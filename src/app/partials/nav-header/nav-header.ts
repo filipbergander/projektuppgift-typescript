571d35f0c9f097fceb199cbb0e0ca48ada788169
Filip Bergander <filip.bergander1996@gmail.com>
2026-05-14 14:32:07 +0200

    Ikon för länken till Hem i navigeringsmenyn.

--- src/app/partials/nav-header/nav-header.ts
+++ src/app/partials/nav-header/nav-header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIcon],
   templateUrl: './nav-header.html',
   styleUrl: './nav-header.scss',
 })
