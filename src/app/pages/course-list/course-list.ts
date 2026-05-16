04c5f756e4e4266f5779af6b5f1281f78bf4f6a8
Filip Bergander <filip.bergander1996@gmail.com>
2026-05-16 16:07:06 +0200

    Responsiv selectbox för färgtema, styrt ned det till strax under 300px för header. Visar enbart ikoner för teman på mobilläge.

--- src/app/pages/course-list/course-list.ts
+++ src/app/pages/course-list/course-list.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { CourseTable } from "../../partials/course-table/course-table";
-import { SelectTheme } from '../../partials/select-theme/select-theme';
 
 @Component({
   selector: 'app-course-list',
-  imports: [CourseTable,SelectTheme],
+  imports: [CourseTable],
   templateUrl: './course-list.html',
   styleUrl: './course-list.scss',
 })
--- src/app/partials/nav-header/nav-header.ts
+++ src/app/partials/nav-header/nav-header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SelectTheme } from '../select-theme/select-theme';
 
 @Component({
   selector: 'app-nav-header',
-  imports: [RouterLink, RouterLinkActive, MatIcon],
+  imports: [RouterLink, RouterLinkActive, MatIcon, SelectTheme],
   templateUrl: './nav-header.html',
   styleUrl: './nav-header.scss',
 })
